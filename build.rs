0f1e5612d293957a9b34d3bfa729230d093c4981
Alfred <alfred_e@outlook.com>
2024-03-31 10:51:52 +0000

    Fixed build.rs and updated docker file

--- build.rs
+++ build.rs
@@ -4,13 +4,15 @@ use {
 };
 
 fn main() -> io::Result<()> {
-    WindowsResource::new()
-        .set_icon("assets/application.ico")
-        .set_version_info(
-            winresource::VersionInfo::PRODUCTVERSION,
-            0x0001_0000_0000_0000,
-        )
-        .compile()?;
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            .set_icon("assets/application.ico")
+            .set_version_info(
+                winresource::VersionInfo::PRODUCTVERSION,
+                0x0001_0000_0000_0000,
+            )
+            .compile()?;
+    }
     let mut out_dir = env::var("OUT_DIR").unwrap();
     out_dir.push_str("../../../../tracer.ino");
     let _ = fs::copy("./assets/tracer/tracer.ino", out_dir);
